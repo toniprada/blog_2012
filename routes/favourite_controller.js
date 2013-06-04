f0b3b2c1b13413f58053ac84ba57a6a81d876b91
Antonio Prada <toniprada@gmail.com>
2013-06-04 13:40:47 +0200

    favourites with a where clause and using posts/index. Patch for heroku

--- routes/favourite_controller.js
+++ routes/favourite_controller.js
@@ -5,7 +5,7 @@ var models = require('../models/models.js');
 
 
 // GET /users/:userid/favourites
-exports.index = function(req, res, next) {
+/*exports.index = function(req, res, next) {
 
     var format = req.params.format || 'html';
     format = format.toLowerCase();
@@ -43,7 +43,68 @@ exports.index = function(req, res, next) {
         .error(function(error) {
             next(error);
         });
-};
+};*/
+
+exports.index = function(req, res, next) {
+
+    var format = req.params.format || 'html';
+    format = format.toLowerCase();
+
+ models.Favourite.findAll({where: {userId: req.user.id}})
+     .success(function(favourites) {
+
+         // generar array con postIds de los post favoritos
+         var postIds = favourites.map( 
+                            function(favourite) 
+                              {return favourite.postId;}
+                           );
+
+        // busca los posts identificados por array postIds
+        var patch;
+        if (postIds.length == 0) {
+            patch= '"Posts"."id" in (NULL)';
+        } else {
+            patch='"Posts"."id" in ('+postIds.join(',')+')';
+        } 
+        // busca los posts identificados por array postIds
+        models.Post.findAll({order: 'updatedAt DESC',
+                    where: patch, 
+                    include:[{model:models.User,as:'Author'},
+                    {model:models.Favourite,as:'Favourite'} ]
+                 })
+                 .success(function(posts) {
+
+          // console.log(posts);
+          
+            switch (format) { 
+              case 'html':
+              case 'htm':
+                  res.render('posts/index', {
+                    posts: posts
+                  });
+                  break;
+              case 'json':
+                  res.send(posts);
+                  break;
+              case 'xml':
+                  res.send(posts_to_xml(posts));
+                  break;
+              case 'txt':
+                  res.send(posts.map(function(post) {
+                      return post.title+' ('+post.body+')';
+                  }).join('\n'));
+                  break;
+              default:
+                  console.log('No se soporta el formato \".'+format+'\" pedido para \"'+req.url+'\".');
+                  res.send(406);
+            }
+        })
+        .error(function(error) {
+            next(error);
+        });
+      });
+    };
+
 
 // POST /posts
 exports.put = function(req, res, next) {
